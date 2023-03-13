353da4dddf36f823b18a0a11eb9c65b5e107dc12
ekodevelops <ekodevelops@gmail.com>
2023-03-12 21:53:59 -0700

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,16 @@ function App() {
 	};
 
 	return (
-		<div className="h-screen max-w-5xl mx-auto py-4 bg-white dark:bg-slate-900 transition duration-500 ease-in-out">
-			<button
-				className="bg-purple-500 p-4 rounded-3xl"
-				onClick={handleThemeChange}
-			>
-				Change theme
-			</button>
+		<div className="w-screen bg-white dark:bg-slate-900 text-gray-700 dark:text-gray-200 transition duration-500 ease-in-out">
+			<div className="h-screen max-w-5xl mx-auto py-4 flex flex-col justify-center items-center">
+				<h1 className="font-bold text-4xl mb-4">Hello World</h1>
+				<button
+					className="bg-purple-500 p-4 rounded-3xl text-gray-200  transition"
+					onClick={handleThemeChange}
+				>
+					Change theme
+				</button>
+			</div>
 		</div>
 	);
 }
