aa468f5c2963162f933e9a957c040988fc961a8c
ekodevelops <ekodevelops@gmail.com>
2023-03-12 21:41:42 -0700

    add dark mode toggler logic example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+	const [theme, setTheme] = useState('dark');
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+	const handleThemeChange = () => {
+		if (theme === 'dark') {
+			setTheme('light');
+		} else {
+			setTheme('dark');
+		}
+	};
+
 	return (
-		<div className="h-screen bg-purple-500 flex justify-center items-center">
-			<h1 className="text-3xl font-bold text-white">Hello World</h1>
+		<div className="h-screen max-w-5xl mx-auto py-4 bg-white dark:bg-slate-900 transition duration-500 ease-in-out">
+			<button
+				className="bg-purple-500 p-4 rounded-3xl"
+				onClick={handleThemeChange}
+			>
+				Change theme
+			</button>
 		</div>
 	);
 }
