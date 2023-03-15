2a53267a6b49ecb7e8e48ed0daed520057105b6d
eko-develops <ekodevelops@gmail.com>
2023-03-15 15:24:56 -0700

    refactor promotions

--- src/components/Promotions.tsx
+++ src/components/Promotions.tsx
@@ -9,24 +9,21 @@ const Promotions = ({
 	return (
 		<div>
 			<h1 className="text-2xl font-bold mb-4">Promotions</h1>
-			<div className="">
-				{promotions &&
-					promotions.map((promotion, index) => (
-						<li className="list-none" key={index}>
-							<Promotion
-								id={promotion.id}
-								title={promotion.title}
-								description={promotion.description}
-								datePosted={promotion.date_posted}
-								startDate={promotion.start_date}
-								endDate={promotion.end_date}
-								scheduled={promotion.scheduled}
-								handleSchedule={handleSchedule}
-								handleDelete={handleDelete}
-							/>
-						</li>
-					))}
-			</div>
+			{promotions &&
+				promotions.map((promotion, index) => (
+					<Promotion
+						key={index}
+						id={promotion.id}
+						title={promotion.title}
+						description={promotion.description}
+						datePosted={promotion.date_posted}
+						startDate={promotion.start_date}
+						endDate={promotion.end_date}
+						scheduled={promotion.scheduled}
+						handleSchedule={handleSchedule}
+						handleDelete={handleDelete}
+					/>
+				))}
 		</div>
 	);
 };
